eb96734466aafd883bbd53cf1e2cd64319f8bc15
mlogallo <85250289+mlogallo@users.noreply.github.com>
2022-03-28 13:23:52 -0400

    Update instance.tf

--- vm_provision/instance.tf
+++ vm_provision/instance.tf
@@ -61,6 +61,7 @@ resource "vsphere_virtual_machine" "vm_deploy" {
   disk {
     label            = "disk0"
     size             = data.vsphere_virtual_machine.template.disks.0.size
+    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
     thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
   }
 
