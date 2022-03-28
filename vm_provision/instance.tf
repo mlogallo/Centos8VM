35c595f40ffb53a44009cf5a14b6faf0207436c5
mlogallo <85250289+mlogallo@users.noreply.github.com>
2022-03-28 13:50:24 -0400

    Update instance.tf

--- vm_provision/instance.tf
+++ vm_provision/instance.tf
@@ -58,6 +58,7 @@ resource "vsphere_virtual_machine" "vm_deploy" {
   network_interface {
     network_id   = data.vsphere_network.network.id
     adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+    mac_address = "00:50:56:9b:bc:66"
   }
 
   disk {
